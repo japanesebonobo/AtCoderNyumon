b3e6a3c3053c816fe92ecbf656e5042cd97775c2
japanesebonobo <tomi119119@gmail.com>
2024-01-04 17:44:56 +0900

    fix otoshidama

--- chapter5/otoshidama.py
+++ chapter5/otoshidama.py
@@ -1,10 +1,12 @@
 def calculator(N, Y):
     for i in range(N+1):
         for j in range(N+1):
-            if (Y == 10000*i+5000*j+1000*(N - i - j)):
-                    a, b, c = i, j, N - i - j
-                    return a, b, c
-
+            k = N - i - j
+            if (k >= 0):
+                if (10000*i+5000*j+1000*k==Y):
+                    return i, j, k
+    return -1, -1, -1
+    
 N, Y = map(int, input().split())
 a, b, c = calculator(N, Y)
 print(a, b, c)
